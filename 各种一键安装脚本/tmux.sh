7cbfbe36b627725a1bb155ab47521595004ee272
黄鸿飞 <853885165@qq.com>
2016-09-01 22:19:48 +0800

    201609012219
    201609012219

---
+++ 各种一键安装脚本/tmux.sh
@@ -0,0 +1,25 @@
+# Install tmux on Centos
+
+
+# install deps
+yum -y install gcc kernel-devel make ncurses-devel
+
+# DOWNLOAD SOURCES FOR LIBEVENT AND MAKE AND INSTALL
+curl -OL https://github.com/downloads/libevent/libevent/libevent-2.0.21-stable.tar.gz
+tar zxf libevent-2.0.21-stable.tar.gz
+cd libevent-2.0.21-stable
+./configure --prefix=/usr/local
+make && make install
+
+
+# DOWNLOAD SOURCES FOR TMUX AND MAKE AND INSTALL
+curl -OL https://github.com/tmux/tmux/releases/download/1.9a/tmux-1.9a.tar.gz
+tar zxf tmux-1.9a.tar.gz
+cd tmux-1.9a
+LDFLAGS="-L/usr/local/lib -Wl,-rpath=/usr/local/lib" ./configure --prefix=/usr/local
+make && make install 
+
+# pkill tmux
+# close your terminal window (flushes cached tmux executable)
+# open new shell and check tmux version
+tmux -V
