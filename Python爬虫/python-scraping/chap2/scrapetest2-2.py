df906c8dfad139c0d48ec9f464bc212373e9e3c1
黄鸿飞 <853885165@qq.com>
2016-10-12 17:19:57 +0800

    201610121719

---
+++ Python爬虫/python-scraping/chap2/scrapetest2-2.py
@@ -0,0 +1,11 @@
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+
+url = "http://www.pythonscraping.com/pages/page3.html"
+
+html = urlopen(url)
+bsObj = BeautifulSoup(html.read(),"html.parser")
+
+# print(bsObj)
+for sibling in bsObj.find("table",{"id":"giftList"}).tr.next_siblings:
+    print(sibling)
